d5f6b324e3ab6f6932186050f64f3cbc3890ec10
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-23 19:10:50 +0300

    fix: rename logo-2.png => logo-white.png

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+const Header = () => (
+  <div className="header">
+    <div className="header__logo">
+      <img src="./img/logo" alt="" />
+    </div>
+  </div>
+);
+
+export default Header;
