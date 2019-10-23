f8b3678e734369b732b75db885fc27ee60dc52cf
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-24 01:34:39 +0300

    feat: add Header, media and icons to him

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Layout from "./components/Layout";
+import Header from "./components/Header";
 
 const App = () => (
   <div>
+    <Header />
     <Layout />
     <Footer />
   </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,65 @@
 import React from "react";
 
-import style from "./header.module.css";
+import styles from "./header.module.css";
+
+import Navigate from "../Navigate";
+import Basket from "../../icons/Basket";
+import Star from "../../icons/Star";
+import Search from "../../icons/Search";
+import Pinterest from "../../icons/Pinterest";
+import Facebook from "../../icons/Facebook";
+import Twitter from "../../icons/Twitter";
+import Instagram from "../../icons/Instagram";
 
 const Header = () => (
-  <div className={style.wrapper}>
-    <div className={style.logo}>
-      <img src="./img/logo" alt="" />
+  <div className={styles.wrapper}>
+    <div className={styles.section}>
+      <div className={styles.logo}>
+        <img src="./img/logo-black.png" alt="logotype image" />
+      </div>
+      <Navigate className={styles} />
+      <div className={styles.btnNewProduct}>New this week</div>
+      <div className={styles.btnItem}>
+        <div className={styles.btnIcon}>
+          <Basket />
+        </div>
+        Cart
+        <span className={styles.count}>(0)</span>
+      </div>
+      <div className={styles.btnItem}>
+        <div className={styles.btnIcon}>
+          <Star />
+        </div>
+        Favorite
+      </div>
+      <div className={styles.btnItem}>
+        <div className={styles.btnIcon}>
+          <Search />
+        </div>
+        Search
+      </div>
+      <div className={styles.social}>
+        <a className={styles.socialLink} href="#">
+          <Pinterest />
+        </a>
+        <a className={styles.socialLink} href="#">
+          <Facebook />
+        </a>
+        <a className={styles.socialLink} href="#">
+          <Twitter />
+        </a>
+        <a className={styles.socialLink} href="#">
+          <Instagram />
+        </a>
+      </div>
+    </div>
+    <div className={styles.section}>
+      <img
+        className={styles.logoMobil}
+        src="./img/logo-black.png"
+        alt="logotype image"
+      />
+      <div className={styles.btnMenu} />
     </div>
   </div>
 );
