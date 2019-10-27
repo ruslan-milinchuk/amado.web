bb06f44721bb4312cf17c98ccc213a0942c8b419
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-27 13:43:17 +0200

    feat: use thunk, routerMiddleware and logger

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import createBrowserHistory from "history/createBrowserHistory";
+import { routerMiddleware } from "react-router-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import rootReducer from "./reducers/index";
 
-const store = createStore(
-  rootReducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const history = createBrowserHistory();
+
+const enhancer = applyMiddleware(thunk, routerMiddleware(history), logger);
+
+const store = createStore(rootReducer, enhancer);
+
+window.store = store;
+
 export default store;
