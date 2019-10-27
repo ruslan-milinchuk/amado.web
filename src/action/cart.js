629546d55ca3eb0d4b638b738ba200969dab0710
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-27 13:40:53 +0200

    feat: add component withScroll, icone ThreeArrows, actions and reducers for Cart

---
+++ src/action/cart.js
@@ -0,0 +1,52 @@
+import {
+  ADD_TO_CART,
+  CHANGE_QTY_PRODUCT,
+  CHANGE_QTY_PRODUCT_DOWN,
+  CHANGE_QTY_PRODUCT_UP,
+  SET_QTY_ENTER_VALUE
+} from "../constants";
+
+export const addToCart = idProduct => {
+  return (dispatch, getState) => {
+    const { cart } = getState();
+    const { qtyProduct } = cart;
+    dispatch({ type: ADD_TO_CART, payload: { idProduct, qtyProduct } });
+  };
+};
+
+export const changeQtyProduct = controlQty => {
+  return (dispatch, getState) => {
+    const { cart } = getState();
+    const { qtyProduct } = cart;
+    if (controlQty === CHANGE_QTY_PRODUCT_UP) {
+      const newQty = parseInt(qtyProduct + 1);
+      if (qtyProduct + 1 <= 300) {
+        dispatch({ type: CHANGE_QTY_PRODUCT, payload: newQty });
+      }
+    }
+
+    if (controlQty === CHANGE_QTY_PRODUCT_DOWN) {
+      const newQty = parseInt(qtyProduct - 1);
+      if (qtyProduct - 1 > 0) {
+        dispatch({ type: CHANGE_QTY_PRODUCT, payload: newQty });
+      }
+    }
+  };
+};
+
+export const changeEnteredValue = enteredValue => {
+  return (dispatch, getState) => {
+    const { cart } = getState();
+    const { qtyProduct } = cart;
+    if (enteredValue > 0 && enteredValue <= 300) {
+      dispatch({ type: SET_QTY_ENTER_VALUE, payload: enteredValue });
+    }
+    if (enteredValue > 300) {
+      dispatch({ type: SET_QTY_ENTER_VALUE, payload: qtyProduct });
+    }
+
+    if (!enteredValue) {
+      dispatch({ type: SET_QTY_ENTER_VALUE, payload: "" });
+    }
+  };
+};
