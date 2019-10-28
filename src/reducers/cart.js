64990c230a13a497fe7a758b2a6f7e474ed08b3b
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-28 13:28:42 +0200

    feat: add func getProductDetails, modificate action and reduser to Cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,23 +1,41 @@
 import {
   ADD_TO_CART,
   SET_QTY_ENTER_VALUE,
-  CHANGE_QTY_PRODUCT
+  CHANGE_QTY_PRODUCT,
+  GET_PRODUCTS_DETAILS_CART
 } from "../constants";
 
-const BASIC_STATE = { qtyProduct: "", cartList: [] };
+const BASIC_STATE = { qtyProduct: "", cartList: [], cartWithDetails: [] };
 
 export const cart = (state = BASIC_STATE, { type, payload }) => {
   switch (type) {
     case SET_QTY_ENTER_VALUE:
-      return { ...state, qtyProduct: payload };
-
     case CHANGE_QTY_PRODUCT:
       return { ...state, qtyProduct: payload };
 
     case ADD_TO_CART:
       const { cartList } = state;
       const { idProduct, qtyProduct } = payload;
-      return { ...state, cartList: [...cartList, [idProduct, qtyProduct]] };
+      if (cartList.findIndex(item => item.id === idProduct) === -1) {
+        return {
+          ...state,
+          cartList: [...cartList, { id: idProduct, qty: qtyProduct }]
+        };
+      }
+
+      if (cartList.findIndex(item => item.id === idProduct) !== -1) {
+        const newState = { ...state };
+        newState.cartList.map(item => {
+          if (item.id === idProduct) {
+            item.qty = qtyProduct;
+          }
+          return newState;
+        });
+      }
+
+    case GET_PRODUCTS_DETAILS_CART:
+      const { detailsProduct } = payload;
+      return { ...state, detailsProduct };
 
     default:
       return state;
