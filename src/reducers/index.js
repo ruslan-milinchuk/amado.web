028ee9ff8bc001da74a75244fe532abf2b9a69f1
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-30 15:49:15 +0200

    feat: connect redux-form, create Checkout and media to him, add components MasterCard and Visa

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,14 @@ import { header } from "./header";
 import { home } from "./home";
 import { product } from "./product";
 import { cart } from "./cart";
+import { reducer as formReducer } from "redux-form";
 
-const rootReducer = combineReducers({ header, home, product, cart });
+const rootReducer = combineReducers({
+  header,
+  home,
+  product,
+  cart,
+  form: formReducer
+});
 
 export default rootReducer;
