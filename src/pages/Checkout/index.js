f4ba27c5b17ed437c827ced381dcd8a0768f1f2c
ruslan-milinchuk <milinchuk.ruslan@gmail.com>
2019-10-31 10:56:17 +0200

    fix: change if/else to ternary operator, do CartTotal just UI component, delete console.log, use validation on field

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,21 +1,156 @@
-import React from "react";
+import React, { Component } from "react";
 
 import styles from "./checkout.module.css";
 import { Field, reduxForm } from "redux-form";
 import CartTotal from "../../components/CartTotal";
 
+import { setCountryList } from "../../action/checkout";
+import { requiredField, maxLengthCreator } from "../../utils/validators";
+import {
+  COMMENT,
+  COMPANY,
+  DELIVERY,
+  EMAIL,
+  INPUT,
+  IS_MAX_LENGTH,
+  MAX_LENGTH_SYMBOL,
+  REQUIRED_FIELD,
+  SELECT,
+  TEXTAREA
+} from "../../constants";
+import { connect } from "react-redux";
+
+import { cartTotal } from "../../utils/cartTotal";
+
 const Checkout = () => {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Checkout</h2>
-      <ReduxCheckoutForm onSubmit={onSubmit} />
+      <ReduxFormWithList onSubmit={onSubmit} />
     </div>
   );
 };
 
-const onSubmit = formData => {
-  console.log("form data", formData);
+const onSubmit = formData => {};
+
+const maxLength = maxLengthCreator(MAX_LENGTH_SYMBOL);
+
+const Form = props => {
+  const { handleSubmit, cartList, history } = props;
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <div className={styles.inputs}>
+        {inputsData.map(
+          ({ name, type, component, placeholder, className }, index) => (
+            <div
+              key={index}
+              className={`${styles.fieldWrapper} ${styles[name]}`}
+            >
+              <Field
+                className={`${styles.field} ${styles[component]}`}
+                name={name}
+                type={type}
+                component={
+                  (component === INPUT && Input) ||
+                  (component === TEXTAREA && TextArea) ||
+                  (component === SELECT && SelectedSetList) ||
+                  component
+                }
+                placeholder={placeholder}
+                validate={
+                  name === COMMENT || name === EMAIL || name === COMPANY
+                    ? [maxLength]
+                    : [maxLength, requiredField]
+                }
+              />
+            </div>
+          )
+        )}
+        <label className={styles.label} htmlFor="create-account">
+          <Field
+            className={styles.checkbox}
+            id="create-account"
+            type="checkbox"
+            name="create-account"
+            component="input"
+          />
+          <i />
+          Create an accout
+        </label>
+        <label className={styles.label} htmlFor="ship-address">
+          <Field
+            className={styles.checkbox}
+            id="ship-address"
+            type="checkbox"
+            name="ship-address"
+            component="input"
+          />
+          <i />
+          Ship to a different address
+        </label>
+      </div>
+      <CartTotal
+        subtotal={cartTotal(cartList).subtotal.toFixed(2)}
+        total={cartTotal(cartList).total.toFixed(2)}
+        delivery={DELIVERY}
+        checkout={true}
+        history={history}
+      />
+    </form>
+  );
 };
+
+const Input = ({ input, meta, ...props }) => {
+  return (
+    <div className={styles.inputWrapper}>
+      <input {...input} {...props} />
+      <span
+        className={
+          (meta.error === REQUIRED_FIELD && meta.touched) ||
+          meta.error === IS_MAX_LENGTH
+            ? `${styles.errorTrue}`
+            : `${styles.errorFalse}`
+        }
+      >
+        {meta.error}
+      </span>
+    </div>
+  );
+};
+
+const TextArea = ({ input, meta, ...props }) => {
+  const hasError = meta.error;
+  return (
+    <div className={styles.inputWrapper}>
+      <textarea {...input} {...props} />
+      <span
+        className={hasError ? `${styles.errorTrue}` : `${styles.errorFalse}`}
+      >
+        {meta.error}
+      </span>
+    </div>
+  );
+};
+
+class Select extends Component {
+  componentDidMount() {
+    const { setCountryList } = this.props;
+    setCountryList();
+  }
+
+  render() {
+    const { countriesList, input, meta, ...props } = this.props;
+    return (
+      <select {...props} {...input}>
+        {countriesList.map(({ name, id }) => (
+          <option key={id} name={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+    );
+  }
+}
 
 const inputsData = [
   {
@@ -47,11 +182,11 @@ const inputsData = [
     className: styles.field
   },
   {
-    name: "us",
-    placeholder: "United States",
-    component: "input",
+    name: "country",
+    placeholder: "Select your country",
+    component: "select",
     type: "text",
-    className: styles.field
+    className: styles.select
   },
   {
     name: "address",
@@ -90,53 +225,18 @@ const inputsData = [
   }
 ];
 
-const CheckoutForm = props => {
-  const { handleSubmit } = props;
-  return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <div className={styles.inputs}>
-        {inputsData.map(
-          ({ name, type, component, placeholder, className }, index) => (
-            <div className={`${styles.fieldWrapper} ${styles[name]}`}>
-              <Field
-                className={`${styles.field} ${styles[component]}`}
-                key={index}
-                name={name}
-                type={type}
-                component={component}
-                placeholder={placeholder}
-              />
-            </div>
-          )
-        )}
-        <label className={styles.label} htmlFor="create-account">
-          <Field
-            className={styles.checkbox}
-            id="create-account"
-            type="checkbox"
-            name="create-account"
-            component="input"
-          />
-          <i />
-          Create an accout
-        </label>
-        <label className={styles.label} htmlFor="ship-address">
-          <Field
-            className={styles.checkbox}
-            id="ship-address"
-            type="checkbox"
-            name="ship-address"
-            component="input"
-          />
-          <i />
-          Ship to a different address
-        </label>
-      </div>
-      <CartTotal checkout={true} />
-    </form>
-  );
-};
+const SelectedSetList = connect(
+  ({ checkout }) => ({ countriesList: checkout.countriesList }),
+  { setCountryList }
+)(Select);
 
-const ReduxCheckoutForm = reduxForm({ form: "checkout" })(CheckoutForm);
+const ReduxForm = reduxForm({ form: "checkout" })(Form);
+
+const ReduxFormWithList = connect(
+  ({ cart }) => ({
+    cartList: cart.cartList
+  }),
+  {}
+)(ReduxForm);
 
 export default Checkout;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { header } from "./header";
 import { home } from "./home";
 import { product } from "./product";
 import { cart } from "./cart";
+import { checkout } from "./checkout";
 import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
@@ -10,6 +11,7 @@ const rootReducer = combineReducers({
   home,
   product,
   cart,
+  checkout,
   form: formReducer
 });
 
